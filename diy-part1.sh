d2f138eeb43b42776f3aa6d754639e73cfead79e
Eterance <eterance@outlook.com>
2023-10-10 02:03:41 +0800

    Update diy-part1.sh xmurp-ua
    xmurp-ua

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
+git clone https://github.com/CHN-beta/xmurp-ua package/xmurp-ua
 git clone https://github.com/Zxilly/UA2F package/UA2F
